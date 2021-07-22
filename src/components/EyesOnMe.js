24fe3fbe565217e86533cfa855ab1123e8aea6e1
Seungil <schoi0115@gmail.com>
2021-07-21 23:41:33 -0400

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+function onBlur(){
+    return(
+        console.log('Hey! Eyes on me!')
+    )
+}
+
+function onFocus(){
+    return(
+        console.log('Good!')
+    )
+}
+
+function EyesOnMe(){
+    return(
+        <button onBlur={onBlur} onFocus={onFocus}>
+            "Eyes on me"
+        </button>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad(){
+    function enter(){
+        console.log("Entering password...")
+
+    }
+    
+    return(
+        <input type="password" onChange={enter}/>
+    )
+}
+
+export default Keypad
